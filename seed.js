74c672d2ac5bd0a98c5807a73310e041af8d4c4b
jengleberg <jeffengleberg@hotmail.com>
2017-10-11 18:37:54 -0600

    fixes seeding issue

--- seed.js
+++ seed.js
@@ -1,30 +1,24 @@
 // This file allows us to seed our application with data
 // simply run: `node seed.js` from the root of this project folder.
 
-var db = require('./models');
+let db = require('./models');
 
-var workout_list = [
+let workout_list = [
 	{
 		exercise: "Bench Press",
   		sets: 3,
   		reps: 8,
-  		body_part: "Pectorals"
+  		body_part: "Chest"
 
 	}
-
-
-
-
-
 ];
 
-// var new_campsite = {description: "Sharp rocks. Middle of nowhere."}
 
-// db.Campsite.create(new_campsite, function(err, campsite){
-//   if (err){
-//     return console.log("Error:", err);
-//   }
+db.Workout.create(workout_list, function(err, workout){
+   if (err){
+     return console.log("Error:", err);
+   }
 
-//   console.log("Created new campsite", campsite._id)
-//   process.exit(); // we're all done! Exit the program.
-// })
+   console.log("Created " + workout._id);
+   process.exit(); // we're all done! Exit the program.
+ });
